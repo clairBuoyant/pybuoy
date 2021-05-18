a46a77a41d8da04aa7864718833f3f4fc8bd9be8
kylejb <47502769+kylejb@users.noreply.github.com>
2021-05-17 23:25:31 -0400

    Add meteorological and wave units from dict

--- src/buoypy/models/buoydata/buoydata.py
+++ src/buoypy/models/buoydata/buoydata.py
@@ -2,25 +2,71 @@
 from dataclasses import dataclass
 from datetime import date
 from .base import BuoyDataBase
+from .unit_mapping import WAVE_UNITS, METEOROLOGICAL
 
 
 @dataclass
 class Meteorological:
-    date_recorded: date = BuoyDataBase(None)
-    wind_direction: int = BuoyDataBase(None)
-    wind_speed: float = BuoyDataBase(None)
-    wind_gust: float = BuoyDataBase(None)
-    wave_height: float = BuoyDataBase(None)
-    dom_wave_period: float = BuoyDataBase(None)
-    avg_wave_period: float = BuoyDataBase(None)
-    wave_direction: int = BuoyDataBase(None)
-    sea_pressure: float = BuoyDataBase(None)
-    air_temperature: float = BuoyDataBase(None)
-    water_temperature: float = BuoyDataBase(None)
-    tide: float = BuoyDataBase(None)
-    dewpoint_temperature: float = BuoyDataBase(None)
-    visibility: float = BuoyDataBase(None)
-    pressure_tendency: float = BuoyDataBase(None)
+    date_recorded: date = BuoyDataBase(
+        None,
+        METEOROLOGICAL["date_recorded"],
+    )
+    wind_direction: int = BuoyDataBase(
+        None,
+        METEOROLOGICAL["wind_direction"],
+    )
+    wind_speed: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["wind_speed"],
+    )
+    wind_gust: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["wind_gust"],
+    )
+    wave_height: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["wave_height"],
+    )
+    dom_wave_period: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["dom_wave_period"],
+    )
+    avg_wave_period: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["avg_wave_period"],
+    )
+    wave_direction: int = BuoyDataBase(
+        None,
+        METEOROLOGICAL["wave_direction"],
+    )
+    sea_pressure: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["sea_pressure"],
+    )
+    air_temperature: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["air_temperature"],
+    )
+    water_temperature: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["water_temperature"],
+    )
+    tide: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["tide"],
+    )
+    dewpoint_temperature: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["dewpoint_temperature"],
+    )
+    visibility: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["visibility"],
+    )
+    pressure_tendency: float = BuoyDataBase(
+        None,
+        METEOROLOGICAL["pressure_tendency"],
+    )
 
 
 @dataclass
@@ -30,27 +76,88 @@ class Spectral(BuoyDataBase):
 
 @dataclass
 class Wave:
-    sea_surface_wave_significant_height: float = BuoyDataBase(None)  # (m)
-    sea_surface_wave_peak_period: float = BuoyDataBase(None)  # (s)
-    sea_surface_wave_mean_period: float = BuoyDataBase(None)  # (s)
-    sea_surface_swell_wave_significant_height: float = BuoyDataBase(None)  # (m)
-    sea_surface_swell_wave_period: float = BuoyDataBase(None)  # (s)
-    sea_surface_wind_wave_significant_height: float = BuoyDataBase(None)  # (m)
-    sea_surface_wind_wave_period: float = BuoyDataBase(None)  # (s)
-    sea_water_temperature: float = BuoyDataBase(None)  # (c)
-    sea_surface_wave_to_direction: float = BuoyDataBase(None)  # (degree)
-    sea_surface_swell_wave_to_direction: float = BuoyDataBase(None)  # (degree)
-    sea_surface_wind_wave_to_direction: float = BuoyDataBase(None)  # (degree)
-    number_of_frequencies: float = BuoyDataBase(None)  # (count)
-    center_frequencies: float = BuoyDataBase(None)  # (Hz)
-    bandwidths: float = BuoyDataBase(None)  # (Hz)
-    spectral_energy: float = BuoyDataBase(None)  # (m**2/Hz)
-    mean_wave_direction: float = BuoyDataBase(None)  # (degree)
-    principal_wave_direction: float = BuoyDataBase(None)  # (degree)
-    polar_coordinate_r1: float = BuoyDataBase(None)  # (1)
-    polar_coordinate_r2: float = BuoyDataBase(None)  # (1)
-    calculation_method = BuoyDataBase(None)  # None
-    sampling_rate: float = BuoyDataBase(None)  # (Hz)
+    sea_surface_wave_significant_height: float = BuoyDataBase(
+        None, WAVE_UNITS["sea_surface_wave_significant_height"]
+    )  # (m)
+    sea_surface_wave_peak_period: float = BuoyDataBase(
+        None, WAVE_UNITS["sea_surface_wave_peak_period"]
+    )  # (s)
+    sea_surface_wave_mean_period: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wave_mean_period"],
+    )  # (s)
+    sea_surface_swell_wave_significant_height: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_swell_wave_significant_height"],
+    )  # (m)
+    sea_surface_swell_wave_period: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_swell_wave_period"],
+    )  # (s)
+    sea_surface_wind_wave_significant_height: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wind_wave_significant_height"],
+    )  # (m)
+    sea_surface_wind_wave_period: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wind_wave_period"],
+    )  # (s)
+    sea_water_temperature: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wind_wave_period"],
+    )  # (c)
+    sea_surface_wave_to_direction: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wind_wave_period"],
+    )  # (degree)
+    sea_surface_swell_wave_to_direction: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_swell_wave_to_direction"],
+    )  # (degree)
+    sea_surface_wind_wave_to_direction: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sea_surface_wind_wave_to_direction"],
+    )  # (degree)
+    number_of_frequencies: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["number_of_frequencies"],
+    )  # (count)
+    center_frequencies: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["center_frequencies"],
+    )  # (Hz)
+    bandwidths: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["bandwidths"],
+    )  # (Hz)
+    spectral_energy: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["spectral_energy"],
+    )  # (m**2/Hz)
+    mean_wave_direction: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["mean_wave_direction"],
+    )  # (degree)
+    principal_wave_direction: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["principal_wave_direction"],
+    )  # (degree)
+    polar_coordinate_r1: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["polar_coordinate_r1"],
+    )  # (1)
+    polar_coordinate_r2: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["polar_coordinate_r2"],
+    )  # (1)
+    calculation_method = BuoyDataBase(
+        None,
+        WAVE_UNITS["calculation_method"],
+    )  # None,
+    sampling_rate: float = BuoyDataBase(
+        None,
+        WAVE_UNITS["sampling_rate"],
+    )  # (Hz)
 
 
 @dataclass
