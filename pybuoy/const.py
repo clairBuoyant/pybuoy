a46eef08c82edc5644fcfba29c88ab2d0f196c89
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2022-07-04 19:15:58 -0400

    release: v0.1.1

--- pybuoy/const.py
+++ pybuoy/const.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 from .endpoints import API_PATH  # noqa: F401
 
-__version__ = "v0.1.1_dev"
+__version__ = "v0.1.1"
 
 
 class Endpoints(Enum):
@@ -13,4 +13,4 @@ class Endpoints(Enum):
 
 #  Enable users to prepend user_agent
 # .format(user_agent)
-USER_AGENT_FORMAT = f"{{}} Buoy{__version__}"
+USER_AGENT_FORMAT = f"{{}} pybuoy-{__version__}"
