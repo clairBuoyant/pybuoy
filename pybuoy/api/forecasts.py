e262685f71b7cd9d60552dbb36edd17abeeb7c98
Aaron Isakov <Aaron@Aarons-MBP-2.attlocal.net>
2022-11-24 20:43:46 -0600

    feat: finally completed implementation of parsing algorithm. Now returns dict of key value pairs with datetime and forecast data.

--- pybuoy/api/forecasts.py
+++ pybuoy/api/forecasts.py
@@ -8,7 +8,6 @@ class Forecasts(ApiBase):
     def get(self, lat: int, lon: int, beginDate: str, endDate: str):
         # TODO: (LOW) add error checking for dates so they are passed in as strings in ISO format
         # If not in ISO format throw user friendly exception
-        # TODO: (LOW) investigate if there's a better way of passing params dict
         response = self.make_request(API_PATH[Endpoints.FORECASTS.value], params={
             "whichClient":"NDFDgen",
             "lat": lat,
@@ -55,21 +54,41 @@ class Forecasts(ApiBase):
         for element_mapping in element_mappings:
             time_layout = next(iter(element_mapping))
             time_stamps = self.__get_values(time_layout, "start-valid-time")
-            mapping_holder = []
+            mapping_holder = {}
             for i, time_stamp in enumerate(time_stamps):
-                timed_mapping = {time_stamp : {}}
-                timed_mapping[time_stamp]["wind_speed_sustained"] = element_mapping[time_layout]["wind_speed_sustained"][i] if "wind_speed_sustained" in element_mapping[time_layout] else None
-                timed_mapping[time_stamp]["wind_speed_gust"] = element_mapping[time_layout]["wind_speed_gust"][i] if "wind_speed_gust" in element_mapping[time_layout] else None
-                timed_mapping[time_stamp]["wind_direction"] = element_mapping[time_layout]["wind_direction"][i] if "wind_direction" in element_mapping[time_layout] else None                    
-                timed_mapping[time_stamp]["wave"] = element_mapping[time_layout]["wave"][i] if "wave" in element_mapping[time_layout] else None
-                mapping_holder.append(timed_mapping)
+                forecast_values = {
+                    "wind_speed_sustained": None,
+                    "wind_speed_gust": None,
+                    "wind_direction": None,
+                    "wave": None
+                }
+
+                if "wind_speed_sustained" in element_mapping[time_layout]:
+                    forecast_values["wind_speed_sustained"] = element_mapping[time_layout]["wind_speed_sustained"][i]
+                if "wind_speed_gust" in element_mapping[time_layout]:
+                    forecast_values["wind_speed_gust"] = element_mapping[time_layout]["wind_speed_gust"][i]
+                if "wind_direction" in element_mapping[time_layout]:
+                    forecast_values["wind_direction"] = element_mapping[time_layout]["wind_direction"][i]
+                if "wave" in element_mapping[time_layout]:
+                    forecast_values["wave"] = element_mapping[time_layout]["wave"][i]
+
+                mapping_holder[time_stamp] = forecast_values
             timed_mappings.append(mapping_holder)
         
-        # Syncing the data to make one big final synced mapping
-        head_timed_mapping = self.__get_longest_list(timed_mappings)
-        
+        synced_timed_mapping = self.__get_longest_mapping(timed_mappings)
+        for timed_mapping in timed_mappings:
+            for key in timed_mapping.keys():
+                if timed_mapping[key]["wind_speed_sustained"] != None:
+                    synced_timed_mapping[key]["wind_speed_sustained"] = timed_mapping[key]["wind_speed_sustained"]
+                if timed_mapping[key]["wind_speed_gust"] != None:
+                    synced_timed_mapping[key]["wind_speed_gust"] = timed_mapping[key]["wind_speed_gust"]
+                if timed_mapping[key]["wind_direction"] != None:
+                    synced_timed_mapping[key]["wind_direction"] = timed_mapping[key]["wind_direction"]
+                if timed_mapping[key]["wave"] != None:
+                    synced_timed_mapping[key]["wave"] = timed_mapping[key]["wave"]
 
-        return timed_mappings
+        print(synced_timed_mapping)
+        return synced_timed_mapping
 
     def __get_values(self, element: Element, value: str) -> list[str]:
         element_text_list = []
@@ -78,11 +97,11 @@ class Forecasts(ApiBase):
             element_text_list.append(element.text)
         return element_text_list
 
-    def __get_longest_list(self, array: list):
+    def __get_longest_mapping(self, array: list) -> dict:
         index = 0
         max_len = 0
-        for i, arr in enumerate(array):
-            if len(arr) > max_len:
+        for i, mapping in enumerate(array):
+            if len(mapping.keys()) > max_len:
                 index = i 
-                max_len = len(arr)
+                max_len = len(mapping.keys())
         return array.pop(index)
